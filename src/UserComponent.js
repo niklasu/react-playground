cc6aa00c9f57094352669a816c7034c2572a327f
Niklas Enns <niklas.enns@gmail.com>
2020-12-23 21:40:34 +0100

    reformar

--- src/UserComponent.js
+++ src/UserComponent.js
@@ -2,40 +2,40 @@ import {Col, Container, ListGroup, ListGroupItem, Row} from "react-bootstrap";
 import {useEffect, useState} from "react";
 
 export function UserComponent() {
-    const [currentUser, setCurrentUser] = useState(null);
-    const [users, setUsers] = useState([]);
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/users`)
-            .then(response => response.json())
-            .then(setUsers)
-    }, []);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then(response => response.json())
+      .then(setUsers)
+  }, []);
 
-    const [currentTodos, setCurrentTodos] = useState([]);
-    useEffect(() => {
-        if (currentUser) {
-            console.log('loading user todos', currentUser.id);
-            fetch(`https://jsonplaceholder.typicode.com/todos`)
-                .then(response => response.json())
-                .then(value => setCurrentTodos(value.filter(t => t.userId === currentUser.id)));
-        }
+  const [currentTodos, setCurrentTodos] = useState([]);
+  useEffect(() => {
+    if (currentUser) {
+      console.log('loading user todos', currentUser.id);
+      fetch(`https://jsonplaceholder.typicode.com/todos`)
+        .then(response => response.json())
+        .then(value => setCurrentTodos(value.filter(t => t.userId === currentUser.id)));
+    }
 
-    }, [currentUser]);
+  }, [currentUser]);
 
-    return <Container>
+  return <Container>
+    <Row>
+      <Col md={4}>
+        <ListGroup>
+          {users.map(u => <ListGroupItem action onClick={() => setCurrentUser(u)}>{u.name}</ListGroupItem>)}
+        </ListGroup>
+      </Col>
+      <Col md={8}>
         <Row>
-            <Col md={4}>
-                <ListGroup>
-                    {users.map(u => <ListGroupItem action onClick={() => setCurrentUser(u)}>{u.name}</ListGroupItem>)}
-                </ListGroup>
-            </Col>
-            <Col md={8}>
-                <Row>
-                    <ListGroup>
-                        {currentTodos.map(t => <ListGroupItem>{t.title}</ListGroupItem>)}
-                    </ListGroup>
-                </Row>
-            </Col>
+          <ListGroup>
+            {currentTodos.map(t => <ListGroupItem>{t.title}</ListGroupItem>)}
+          </ListGroup>
         </Row>
+      </Col>
+    </Row>
 
-    </Container>;
+  </Container>;
 }

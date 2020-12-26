cab3cae1bc96271d1e8a2228260c09c86c902154
Niklas Enns <niklas.enns@gmail.com>
2020-12-26 23:15:09 +0100

    reformat

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import {Route, Switch} from 'react-router-dom';
 import {MainComponent} from "./main/MainComponent";
 import {UserComponent} from "./users/UserComponent";
 import {MyNavBar} from "./MyNavBar";
-import {ReduxBased} from "./hello/Hello";
+import {UseMemoExample} from "./hello/Hello";
 
 function App() {
-    return (
-        <main>
-            <MyNavBar/>
-            <Switch>
-                <Route path="/" component={MainComponent} exact/>
-                <Route path="/users" component={UserComponent} exact/>
-                <Route path="/redux" component={ReduxBased} exact/>
-            </Switch>
-        </main>
-    )
+  return (
+    <main>
+      <MyNavBar/>
+      <Switch>
+        <Route path="/" component={MainComponent} exact/>
+        <Route path="/users" component={UserComponent} exact/>
+        <Route path="/redux" component={UseMemoExample} exact/>
+      </Switch>
+    </main>
+  )
 
 }
 
--- src/MyNavBar.js
+++ src/MyNavBar.js
@@ -1,11 +1,11 @@
 import {Navbar, Nav} from "react-bootstrap";
 
 export function MyNavBar() {
-    return (
-            <Navbar bg={'light'}>
-                <Nav.Link href="/">Main</Nav.Link>
-                <Nav.Link href="/users">Users</Nav.Link>
-                <Nav.Link href="/redux">Another Page</Nav.Link>
-            </Navbar>
-        );
+  return (
+    <Navbar bg={'light'}>
+      <Nav.Link href="/">Main</Nav.Link>
+      <Nav.Link href="/users">Users</Nav.Link>
+      <Nav.Link href="/redux">Another Page</Nav.Link>
+    </Navbar>
+  );
 }

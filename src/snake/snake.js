811f77090e11f192db5d8cb455388171a439ab10
Niklas Enns <niklas.enns@gmail.com>
2020-12-27 14:20:30 +0100

    cut tail of snek

--- src/snake/snake.js
+++ src/snake/snake.js
@@ -2,13 +2,40 @@ import {useEffect, useRef, useState} from "react";
 
 export const Snake = function Snake() {
   const canvasRef = useRef(null);
-  const [x, setX] = useState(200);
-  const [y, setY] = useState(200);
+  const INITIAL_X = 200;
+  const [x, setX] = useState(INITIAL_X);
+  const [y, setY] = useState(INITIAL_X);
   const [currentDirection, setCurrentDirection] = useState('ArrowLeft');
 
+  const [path, setPath] = useState([]);
+
   useEffect(() => {
-    const canvas = canvasRef.current
+    const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d')
+
+    function clear(erem) {
+      console.log(`removing ${erem}`)
+      //ctx.clearRect(erem[0]-10, erem[1]-10, 20, 20);
+      ctx.fillStyle = "#FF0000";
+      ctx.beginPath()
+      //ctx.fillRect(erem[0]-1, erem[1]-1, 10, 2);
+      ctx.clearRect(erem[0]-1, erem[1]-1, 10, 10);
+    }
+
+    let p = path;
+    const pLength = p.length;
+    if (pLength < 20) {
+      p[pLength] = [x, y];
+    } else {
+      clear(p.shift())
+      p = [...p, [x,y]]
+    }
+    setPath(p)
+    //console.log(path);
+
+
+
+    ctx.lineWidth = 2;
     ctx.moveTo(x, y);
     if (currentDirection === 'ArrowLeft') {
       ctx.lineTo(x - 10, y);
